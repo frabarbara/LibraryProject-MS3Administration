15830c11a84f2a9f8af091cff9afab578b98838b
frabarbara <francesco.barbara5697@gmail.com>
2023-11-07 16:52:17 +0100

    init commit

---
+++ src/main/java/it/gammainnovation/ms3administration/Ms3AdministrationApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms3administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms3AdministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms3AdministrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/gammainnovation/ms3administration/Ms3AdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms3administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms3AdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
